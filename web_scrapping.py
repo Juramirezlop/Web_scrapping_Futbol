710dc48bae786ffb68d94dea557f88ef8136588c
Juramirezlop <72463915+Juramirezlop@users.noreply.github.com>
2020-12-10 16:35:34 -0500

    Add files via upload

---
+++ web_scrapping.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+url = "https://www.colombia.com/futbol/liga-colombiana/tabla-de-posiciones"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+titulares = soup.find_all(True, {'class':['d-none d-md-inline']})
+nombres = list(titulares)
+cad = ''
+limit = 0
+acum = ''
+for i in range(len(nombres)):
+  cad += str(nombres[i])
+for i in range(len(cad)):
+  if i == 0:
+    continue
+  elif ord(cad[i]) == 62:
+    limit += 1
+  elif ord(cad[i]) == 60:
+    limit -= 1
+    acum += ' '
+  elif limit == 1:
+    acum += str(cad[i])
+print(acum)
